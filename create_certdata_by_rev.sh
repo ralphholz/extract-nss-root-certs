92a48ea4574aa846098ef1de64270fdfc7b1d858
Ralph Holz <ralph@ralphholz.de>
2013-07-16 13:01:30 +0200

    Added GPL notices

--- create_certdata_by_rev.sh
+++ create_certdata_by_rev.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# This file is part of the SSL Landscape:
+#
+# Ralph Holz, Lothar Braun, Nils Kammenhuber, and Georg Carle.
+# The SSL Landscape - A Thorough Analysis of the X.509 PKI 
+# Using Active and Passive Measurements. In Proc. ACM/USENIX 
+# 11th Annual Internet Measurement Conference (IMC '11), Berlin, 
+# Germany, November 2011
+#
+# It is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This code is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file. If not, see <http://www.gnu.org/licenses/>.
+
+# Code history:
+# Original author: Ralph Holz, ralph@ralphholz.de
+
+
 # You need to execute this script in the directory
 # mozilla/security/nss/lib/ckfw/builtins; checked
 # out as a CVS repository.
